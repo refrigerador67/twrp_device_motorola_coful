b6648662e5ec9804be7fb313235a677c8b78aa6f
Ubuntu <azureuser@LearnVMVishwa01.jathnnng2paejafvec14a3jfta.tx.internal.cloudapp.net>
2022-11-04 16:51:25 +0000

    first commit

---
+++ omni_coful.mk
@@ -0,0 +1,29 @@
+#
+# Copyright (C) 2022 The Android Open Source Project
+# Copyright (C) 2022 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+
+# Inherit from coful device
+$(call inherit-product, device/motorola/coful/device.mk)
+
+PRODUCT_DEVICE := coful
+PRODUCT_NAME := omni_coful
+PRODUCT_BRAND := motorola
+PRODUCT_MODEL := moto g31
+PRODUCT_MANUFACTURER := motorola
+
+PRODUCT_GMS_CLIENTID_BASE := android-motorola
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="coful_g-user 11 RRWBS31.Q3-46-85-7 5bf8c90 release-keys"
+
+BUILD_FINGERPRINT := motorola/coful_g/coful:11/RRWBS31.Q3-46-85-7/5bf8c90:user/release-keys
